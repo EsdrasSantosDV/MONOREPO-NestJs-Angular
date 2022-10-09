3b14675caf53243dbcac127612eaa356609cc691
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2022-10-08 23:14:09 -0300

    feat:products create components/services

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {ProductsController, ProductsService} from "@shirt-shop/products";
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController,ProductsController],
+  providers: [AppService,ProductsService],
 })
 export class AppModule {}
--- apps/shirt-shop/src/app/app.module.ts
+++ apps/shirt-shop/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
+
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [BrowserModule],
