37b0a7de7fe6fb11ce7ee20d9f498e0f2eeec387
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2022-10-09 08:00:13 -0300

    feat:finished the card product

--- apps/shirt-shop/src/app/app.module.ts
+++ apps/shirt-shop/src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import {UiModule} from "@shirt-shop/ui";
 
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule,UiModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- libs/ui/src/lib/products/products.component.ts
+++ libs/ui/src/lib/products/products.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Observable} from "rxjs";
+import {Product} from '@prisma/client';
+import {ProductsService} from "../products.service";
 @Component({
   selector: 'shirt-shop-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent implements OnInit {
-  constructor() {}
+  public $products:Observable<Product[]> | undefined
+  constructor(public productsService:ProductsService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.$products=this.productsService.getProducts();
+  }
+
+
 }
